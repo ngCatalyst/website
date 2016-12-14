1278228ce3efaf2b24649fdb613c88768e49e7bb
Daniel Starkey <danielstarkey@Daniels-MacBook-Pro.local>
2016-12-14 16:54:54 +0000

    Catalyst twitter timeline embedded onto page

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var concat = require('gulp-concat');
 var flatten = require('gulp-flatten');
 var inject = require('gulp-inject');
 var strip = require('gulp-strip-comments');
+var webserver = require('gulp-webserver');
 
 function _transform (filepath, file, i, length) {
     return (filepath.endsWith('js')) ? '<script src="' + filepath.substring(filepath.lastIndexOf('/') + 1) + '"></script>' : '<link rel="stylesheet" type="text/css" href="' + filepath.substring((filepath.lastIndexOf('/') + 1)) + '" />';
@@ -13,9 +14,9 @@ gulp.task('compile', [], function () {
 
     var mainHtmlComponent = 'src/html/index.html';
 
-    var jsComponents = ['src/js/*.js', 'src/js/**/*.js'];
+    var jsComponents = ['src/bower_components/**/dist/*.min.js', 'src/bower_components/**/dist/js/*.min.js', 'src/js/*.js', 'src/js/**/*.js', '!src/bower_components/bootstrap/dist/css/bootstrap-theme.min.css'];
     var htmlComponents = [mainHtmlComponent, 'src/html/**/*.html'];
-    var cssComponents = ['src/css/*.css'];
+    var cssComponents = ['src/bower_components/**/dist/css/*.min.css', 'src/css/*.css'];
 
     gulp.src(htmlComponents).pipe(flatten()).pipe(gulp.dest('dist/app'));
 
@@ -32,6 +33,17 @@ gulp.task('compile', [], function () {
             )
             .pipe(strip())
             .pipe(gulp.dest('dist/app'));
+});
+
+gulp.task('webserver', [], function () {
+    gulp.src('dist').pipe(webserver({
+        livereload: true,
+        port: 9003
+    }));
+});
+
+gulp.task('watch', ['build', 'webserver'], function () {
+    return gulp.watch(['src/html/*.html', 'src/html/**/*.html'], ['build']);
 });
 
 gulp.task('build', ['compile'], function () {
