0a942573d7a62a2e4ac695b17fda8dd0d70f4c2e
Daniel Starkey <danielstarkey@Daniels-MacBook-Pro.local>
2016-12-15 12:38:53 +0000

    Image slider added ed to the homepage to display the latest images to the users. AngularJS has also been introduced to allow for the page to be seperated into templates

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var flatten = require('gulp-flatten');
 var inject = require('gulp-inject');
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
 var strip = require('gulp-strip-comments');
 var webserver = require('gulp-webserver');
 
@@ -9,16 +11,28 @@ function _transform (filepath, file, i, length) {
     return (filepath.endsWith('js')) ? '<script src="' + filepath.substring(filepath.lastIndexOf('/') + 1) + '"></script>' : '<link rel="stylesheet" type="text/css" href="' + filepath.substring((filepath.lastIndexOf('/') + 1)) + '" />';
 };
 
+function _toLower (file) {
+    file.basename = file.basename.toLowerCase();
+};
+
 gulp.task('compile', [], function () {
     var injectConfig = { transform: _transform };
 
     var mainHtmlComponent = 'src/html/index.html';
 
-    var jsComponents = ['src/bower_components/**/dist/*.min.js', 'src/bower_components/**/dist/js/*.min.js', 'src/js/*.js', 'src/js/**/*.js'];
+    var jsComponents = ['src/bower_components/**/dist/*.min.js', 'src/bower_components/**/dist/js/*.min.js', 'src/bower_components/**/*.min.js', 'src/js/*.js', 'src/js/**/*.js'];
     var htmlComponents = [mainHtmlComponent, 'src/html/**/*.html'];
-    var cssComponents = ['!src/bower_components/bootstrap/dist/css/bootstrap-theme.min.css', 'src/bower_components/**/dist/css/*.min.css', 'src/css/*.css'];
+    var cssComponents = ['!src/bower_components/bootstrap/dist/css/bootstrap-theme.min.css', 'src/bower_components/**/dist/css/*.min.css', 'src/bower_components/**/css/*.min.css', 'src/css/*.css'];
+    var fonts = ['src/fonts/*.*ttf'];
+    var images = ['src/images/*.jpg', 'src/images/*.jpeg'];
+    var bowerFonts = ['src/bower_components/**/fonts/*.ttf'];
 
     gulp.src(htmlComponents).pipe(flatten()).pipe(gulp.dest('dist/app'));
+
+    gulp.src(fonts).pipe(flatten()).pipe(rename(_toLower)).pipe(gulp.dest('dist/app'));
+    gulp.src(bowerFonts).pipe(flatten()).pipe(rename(_toLower)).pipe(gulp.dest('dist/fonts'));
+
+    gulp.src(images).pipe(imagemin()).pipe(gulp.dest('dist/app'));
 
     return gulp.src(mainHtmlComponent)
             .pipe(inject(
@@ -43,7 +57,7 @@ gulp.task('webserver', [], function () {
 });
 
 gulp.task('watch', ['build', 'webserver'], function () {
-    return gulp.watch(['src/html/*.html', 'src/html/**/*.html', 'src/css/*.css'], ['build']);
+    return gulp.watch(['src/html/*.html', 'src/html/**/*.html', 'src/js/*.js', 'src/js/**/*.js', 'src/css/*.css', 'src/fonts/*.ttf', 'src/images/*.jpg', 'src/images/*.jpeg'], ['build']);
 });
 
 gulp.task('build', ['compile'], function () {
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,24 @@
 console.log('app.js');
+
+var app = angular.module('catalyst', []);
+
+app.controller('homeCtrl', ['$scope', function ($scope) {
+    var home = this;
+    
+    home.message = 'homeCtrl loaded...';
+
+    $scope.imageSlider = {};
+    $scope.imageSlider.images = [
+        'microsoft-1.jpg',
+        'microsoft-2.jpg',
+        'microsoft-3.jpg',
+        'microsoft-4.jpg',
+    ];
+
+}]);
+
+app.directive('imageSlider', function() {
+  return {
+    templateUrl: 'image-slider.html'
+  };
+});
